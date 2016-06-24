caa2ba787ecf3e733a24c97a2e17895f2f44754e
Aaron <newcomer.aaron@icloud.com>
2016-06-24 15:54:38 -0400

    added new appointment function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
   var appointments = JSON.parse(localStorage.getItem('appointments'))
+  for(var i= 0; i < appointments.length ; i++)
   $(".content").append(
-    '<div class="weather"><p class="time">' + appointments[0].time + '</p></div>' +
-    '<div class="appointment"><div class="aptContent"><p class="aptTitle">' + appointments[0].description +'</p><p class="location">' + appointments[0].street + ' ' + appointments[0].location + '</p></div></div>'
+    '<div class="weather"><p class="time">' + appointments[i].time + '</p></div>' +
+    '<div class="appointment"><div class="aptContent"><p class="aptTitle">' + appointments[i].description +'</p><p class="location">' + appointments[i].street + ' ' + appointments[i].location + '</p></div></div>'
   );
-  $(".content").append(
-    '<div class="weather"><p class="time">' + appointments[1].time + '</p></div>' +
-    '<div class="appointment"><div class="aptContent"><p class="aptTitle">' + appointments[1].description +'</p><p class="location">' + appointments[1].street + ' ' + appointments[1].location + '</p></div></div>'
-  );
+
   console.log(appointments);
   console.log("hello");
 });
@@ -20,7 +18,7 @@ $(".searchBox").keydown(function(){
 $(".search").submit(function(e){
   e.preventDefault();
     console.log($(".searchBox").val());
-    console.log($(".aptTitle").val());
+    // console.log($(".aptTitle").val());
   if ($(".searchBox").val() === $(".aptTitle").text()){
     // console.log($(".searchBox").val());
     // console.log($(".aptTitle").val());
---
+++ assets/scripts/newApt.js
@@ -0,0 +1,38 @@
+$(".saveAptButton").click(function(e){
+  e.preventDefault();
+  var dateJson = $(".textBoxDate").val()
+  var timeJson = $(".textBoxTime").val()
+  var descriptionJson = $(".textBoxTitle").val()
+  var streetJson = $(".textBoxStreet").val()
+  var locationJson = $(".textBoxCityState").val()
+
+  var newApt = {
+    date: dateJson,
+    time: timeJson,
+    description: descriptionJson,
+    street: streetJson,
+    location: locationJson
+  }
+  var oldAppointments = JSON.parse(localStorage.getItem('appointments')) || [];
+  oldAppointments.push(newApt);
+  localStorage.setItem('appointments', JSON.stringify(oldAppointments));
+  console.log(newApt);
+
+})
+
+
+
+// var dateJson = $(".textBoxDate").val()
+// var timeJson = $(".textBoxTime").val()
+// var descriptionJson = $(".textBoxTitle").val()
+// var streetJson = $(".textBoxStreet").val()
+// var locationJson = $(".textBoxCityState").val()
+//
+// var newApt = {
+//   date: dateJson,
+//   time: timeJson,
+//   description: descriptionJson,
+//   street: streetJson,
+//   location: locationJson
+// }
+// localStorage.setItem('appointments', JSON.stringify([{date: "7/05/15", time: "1:00", description: "Lunch with MB", street: "2612 Main st", location: "Pittsboro, NC"},{date:"6/30/15", time: "2:00", description: "Meet w/ Misty", street: "1212 Main St", location: "Durham, NC"}]))
