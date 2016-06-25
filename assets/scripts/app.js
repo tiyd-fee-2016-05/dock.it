4cf9e751c150931c123952252da5c66c0991ae7d
Aaron <newcomer.aaron@icloud.com>
2016-06-25 10:39:26 -0400

    Added link for view & search feature

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  var appointments = JSON.parse(localStorage.getItem('appointments'))
+  appointments = JSON.parse(localStorage.getItem('appointments'))
   for(var i= 0; i < appointments.length ; i++)
   $(".content").append(
-    '<div class="weather"><p class="time">' + appointments[i].time + '</p></div>' +
-    '<div class="appointment"><div class="aptContent"><p class="aptTitle">' + appointments[i].description +'</p><p class="location">' + appointments[i].street + ' ' + appointments[i].location + '</p></div></div>'
+    '<a href="viewApt.html">' + '<div class="weather" id="' + i + '"><p class="time">' + appointments[i].time + '</p></div>' +
+    '<div class="appointment" id="'+ i +'"><div class="aptContent" id="'+ i +'"><p class="aptTitle">' + appointments[i].description +'</p><p class="location">' + appointments[i].street + ' ' + appointments[i].location + '</p></div></div></a>'
   );
 
   console.log(appointments);
@@ -13,15 +13,40 @@ $(".searchBox").keydown(function(){
   $(".searchBox").css("text-align" , "left");
   if ($(".searchBox").val() === "") {
     $(".searchBox").css("text-align", "right");
+    $(".content").html("");
+    for(var i= 0; i < appointments.length ; i++)
+    $(".content").append(
+      '<div class="weather" id="' + i + '"><p class="time">' + appointments[i].time + '</p></div>' +
+      '<div class="appointment" id="'+ i +'"><div class="aptContent" id="'+ i +'"><p class="aptTitle">' + appointments[i].description +'</p><p class="location">' + appointments[i].street + ' ' + appointments[i].location + '</p></div></div>'
+    );
   }
 });
+// for (var x = 0; x <= 4; x++)
+//
+// console.log(x);
+
 $(".search").submit(function(e){
   e.preventDefault();
     console.log($(".searchBox").val());
-    // console.log($(".aptTitle").val());
-  if ($(".searchBox").val() === $(".aptTitle").text()){
-    // console.log($(".searchBox").val());
-    // console.log($(".aptTitle").val());
+     console.log($(".aptTitle").val());
+    // var index = 0
+    // for (var x = 0; x < 4; x++)
+    // var index = index[x];
+    var compare = JSON.parse(localStorage.getItem('appointments'))
+    for (var x = 0; x < compare.length; x++)
+
+    // console.log(compare);
+    // console.log(x);
+    // console.log(document.getElementById(x.stringify()));
+  if ($(".searchBox").val() === compare[x].description){
+    console.log(compare[x]);
+    $(".content").html("")
+    $(".content").append(
+      '<div class="weather" id="' + x + '"><p class="time">' + compare[x].time + '</p></div>' +
+      '<div class="appointment" id="'+ x +'"><div class="aptContent" id="'+ x +'"><p class="aptTitle">' + compare[x].description +'</p><p class="location">' + compare[x].street + ' ' + compare[x].location + '</p></div></div>'
+    );
+
+
     console.log("This works");
   }
 })
