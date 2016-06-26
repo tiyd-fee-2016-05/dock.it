853f6cb2fa3e085b4513cbabec2544c3ef123100
Aaron <newcomer.aaron@icloud.com>
2016-06-26 14:50:48 -0400

    added icon

---
+++ assets/scripts/editApt.js
@@ -0,0 +1,80 @@
+//populate page with info
+appointments = JSON.parse(localStorage.getItem('appointments'));
+var x = JSON.parse(localStorage.getItem('id'));
+$(".textBoxTitle").val(appointments[x].description);
+$(".textBoxStreet").val(appointments[x].street);
+$(".textBoxCityState").val(appointments[x].location);
+$(".textBoxDate").val(appointments[x].date);
+$(".textBoxTime").val(appointments[x].time);
+console.log(appointments);
+console.log(x);
+
+$(document).ready(function(){
+  appointments = JSON.parse(localStorage.getItem('appointments'));
+
+  // if (x > -1) {
+  //   appointments.splice(x, 1);  //inspired by http://stackoverflow.com/questions/5767325/remove-a-particular-element-from-an-array-in-javascript
+  // }
+
+  // localStorage.setItem('appointments', JSON.stringify(appointments));
+  console.log(appointments);
+  $(".saveButton").click(function(e){
+    // e.preventDefault(e);
+      // e.preventDefault();
+      var dateJson = $(".textBoxDate").val()
+      var timeJson = $(".textBoxTime").val()
+      var descriptionJson = $(".textBoxTitle").val()
+      var streetJson = $(".textBoxStreet").val()
+      var locationJson = $(".textBoxCityState").val()
+
+      var editApt = {
+        date: dateJson,
+        time: timeJson,
+        description: descriptionJson,
+        street: streetJson,
+        location: locationJson
+      }
+      //this was inspired by stackoverflow here https://stackoverflow.com/questions/12162786/adding-new-objects-to-localstorage
+      // once again stackoverflow is BAE
+      var oldAppointments = JSON.parse(localStorage.getItem('appointments')) || [];
+      oldAppointments.push(editApt);
+      localStorage.setItem('appointments', JSON.stringify(oldAppointments));
+      console.log(editApt);
+    })
+})
+// var backCount = 0
+// $(".v").click(function(e){
+//   //e.preventDefault(e);
+//   backCount ++;
+//   console.log(backCount);
+//   if (backCount === 1){
+//     e.preventDefault(e);
+//     console.log(backCount);
+//     alert("This will delete this appointment!!!");
+//     // count ++
+//   }
+//   else if (x > -1 && backCount > 1) {
+//     appointments.splice(x, 1);  //inspired by http://stackoverflow.com/questions/5767325/remove-a-particular-element-from-an-array-in-javascript
+//     backCount = 0;
+//     localStorage.setItem('appointments', JSON.stringify(appointments));
+//     console.log(appointments);
+//   }
+// })
+var count = 0
+$(".deleteButton").click(function(e){
+  // e.preventDefault(e);
+  count ++;
+  console.log(count);
+  if (count === 1){
+    e.preventDefault(e);
+    console.log(count);
+    alert("This will delete this appointment!!!");
+    // count ++
+  }
+  else if (x > -1 && count > 1) {
+    appointments.splice(x, 1);  //inspired by http://stackoverflow.com/questions/5767325/remove-a-particular-element-from-an-array-in-javascript
+    count = 0;
+    localStorage.setItem('appointments', JSON.stringify(appointments));
+    console.log(appointments);
+  }
+})
--- assets/scripts/viewApt.js
+++ assets/scripts/viewApt.js
@@ -1,12 +1,14 @@
-
+var x = JSON.parse(localStorage.getItem('id'));
+var cityState = appointments[x].location;
+var split = cityState.split(",");
+console.log(split);
   $(document).ready(function(){
     $.ajax({
       dataType: 'json',
-      url: "http://api.wunderground.com/api/35c9d364b4732721/forecast10day/q/CA/San_Francisco.json",
+      url: "http://api.wunderground.com/api/35c9d364b4732721/forecast10day/q/" + split[1] + "/" + split[0] + ".json", //CA/San_Francisco.json",
       method: "GET",
     }).done(function(json){
 
-          var x = JSON.parse(localStorage.getItem('id'))
 
           var today = new Date();
           var dd = today.getDate();
@@ -33,7 +35,8 @@
       $(".aptContent").append(
         '<p class="dateTime">' + appointments[x].date + ' at <span class="time">' + appointments[x].time +
         '<p class="aptLocation">' + appointments[x].location + '</p>' + '<img class="map" src="http://i.imgur.com/OIotcym.png"/>' +
-        '<div class="forcast"><p class="forcastDetails">' + json.forecast.simpleforecast.forecastday[index].high.fahrenheit + 'F' + '</p></div>'
+        '<div class="forcast"><img class="icon" src="' + json.forecast.simpleforecast.forecastday[index].icon_url + '"/>' +
+        '<p class="forcastDetails">' + json.forecast.simpleforecast.forecastday[index].high.fahrenheit + '&#176F and ' + json.forecast.simpleforecast.forecastday[index].conditions + '</p></div>'
       );
       console.log(json);
       }
@@ -42,13 +45,59 @@
         $(".aptContent").append(
           '<p class="dateTime">' + appointments[x].date + ' at <span class="time">' + appointments[x].time +
           '<p class="aptLocation">' + appointments[x].location + '</p>' + '<img class="map" src="http://i.imgur.com/OIotcym.png"/>' +
-          '<div class="forcast"><p class="forcastDetails" style= "font-size: 1em">' + "Too far in the future" + '</p></div>'
+          '<div class="forcast"><p class="forcastDetails">' + "Too far in the future" + '</p></div>'
         );
 
       }
     });
   });
 
+        // This function was made because wunderground decided to shit the bed. This makes
+        // it function idependently of the API call. Foresight is not my strong suit.
+ //
+ //            $(document).ready(function(){
+ //
+ //            var x = JSON.parse(localStorage.getItem('id'))
+ //
+ //            var today = new Date();
+ //            var dd = today.getDate();
+ //            // var mm = today.getMonth()+1; //January is 0!
+ //            if(dd<10) {
+ //            dd='0'+dd
+ //          }
+ //          //  if(mm<10) {
+ //          //    mm='0'+mm
+ //          // }
+ //          today = dd
+ //          future= appointments[x].date;
+ //          futureCompare = future.slice(3,5);
+ //          console.log(futureCompare);
+ //          futureNumber = parseInt(futureCompare);
+ //          console.log(futureNumber);
+ //          index= futureNumber-today
+ //          $(".aptHeader").append(
+ //            '<p class="aptName">' + appointments[x].description + '</p>'
+ //            )
+ //          console.log(index + "yes?");
+ //        if (index >=0){
+ //
+ //        $(".aptContent").append(
+ //          '<p class="dateTime">' + appointments[x].date + ' at <span class="time">' + appointments[x].time +
+ //          '<p class="aptLocation">' + appointments[x].location + '</p>' + '<img class="map" src="http://i.imgur.com/OIotcym.png"/>' +
+ //          '<div class="forcast"><p class="forcastDetails">' + '&#176F' + '</p></div>'
+ //        );
+ //        // console.log(json);
+ //        }
+ //        else {
+ //
+ //          $(".aptContent").append(
+ //            '<p class="dateTime">' + appointments[x].date + ' at <span class="time">' + appointments[x].time +
+ //            '<p class="aptLocation">' + appointments[x].location + '</p>' + '<img class="map" src="http://i.imgur.com/OIotcym.png"/>' +
+ //            '<div class="forcast"><p class="forcastDetails" style= "font-size: 1em">' + "Too far in the future" + '</p></div>'
+ //          );
+ //
+ //        }
+ // });
 
     // console.log(appointments)
     // appointments = JSON.parse(localStorage.getItem('appointments'))
